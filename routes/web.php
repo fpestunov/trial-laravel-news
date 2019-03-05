ac6d181f926aa00c73eeee4c398cd38c2e2ce23e
Fedor Pestunov <fpestunov@gmail.com>
2019-03-05 19:25:31 +0400

    define URI in route file

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'NewsController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/news/{id}', 'NewsController@show');
+
+Route::get('/manager/news/create', 'NewsController@create');
+
+Route::get('/manager/news/{id}/edit', 'NewsController@edit');
+
+Route::post('/manager/news', 'NewsController@store');
+
+Route::patch('/manager/news/{id}', 'NewsController@update');
+
+Route::delete('/manager/news/{id}', 'NewsController@destroy');
